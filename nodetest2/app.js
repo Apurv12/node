12eb71b26c2c3cca3f4dca587c72eb46c8c541e8
Apurv12 <apurv.acharya@capita.co.uk>
2014-07-23 15:14:45 +0530

    Db switch
    Changed DB localhost configuration to server conf

--- nodetest2/app.js
+++ nodetest2/app.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 
 // Database
 var mongo = require('mongoskin');
-var db = mongo.db("mongodb://localhost:27017/nodetest2", {native_parser:true});
+var db = mongo.db("mongodb://10.105.22.57:27017/nodetest2", {native_parser:true});
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
